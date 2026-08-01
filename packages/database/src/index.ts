0e76ef915980e464c137ebd098e66fd9af1ea4d9
manavjangid5 <manavjangid5@gmail.com>
2026-08-01 22:35:41 +0530

    chore: package/database/src/index.js file log check

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -1,10 +1,14 @@
 import { PrismaClient, Prisma } from "@prisma/client";
 
+console.log("@atlas/database module loading, DATABASE_URL present:", !!process.env.DATABASE_URL);
+
 declare global {
   var __prisma: PrismaClient | undefined;
 }
 
 export const prisma = global.__prisma || new PrismaClient();
+
+console.log("@atlas/database prisma client created:", !!prisma);
 
 if (process.env.NODE_ENV !== "production") {
   global.__prisma = prisma;
